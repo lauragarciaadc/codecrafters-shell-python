6ac75f2876e12fb68713fff98fed8158dc365b4b
Laura Garcia <lauragarciatnf@gmail.com>
2026-03-02 11:40:19 +0100

    Support error messages for invalid commands

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-    pass
+    # Esperar el input del usuario
+    command = input()
+    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
