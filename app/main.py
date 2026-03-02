4fe6f2204cc736a916e9b6c721b35da7c38d6480
Laura Garcia <lauragarciatnf@gmail.com>
2026-03-02 11:48:48 +0100

    Exit builtin

--- app/main.py
+++ app/main.py
@@ -3,10 +3,14 @@ import sys
 
 def main():
     # Esperar el input del usuario
+
     while True:
         sys.stdout.write("$ ")
         command = input()
+        if command == "exit":
+            break
         print(f"{command}: command not found")
+        
 
 
 if __name__ == "__main__":
